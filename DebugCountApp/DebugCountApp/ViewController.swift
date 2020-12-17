0798a96acb8b2bde4915a4cce574c5973b11d2fd
fummicc1 <fumiya.tennis1@gmail.com>
2020-12-17 16:32:43 +0900

    Add DebugCountApp

---
+++ DebugCountApp/DebugCountApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DebugCountApp
+//
+//  Created by fumiyatanaka_admin on 2020/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel
+    @IBOutlet var plusButton: UIButton!
+    
+    var number: String = "0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        designPlusButton()
+    }
+
+    @IBAction func plus() {
+        number += "1"
+        label.text = String(number)
+    }
+    
+    // Plusボタンを角丸にして枠線をつけるメソッド
+    func designPlusButton() {
+        plusButton.layer.cornerRadius = 8
+        plusButton.layer.borderWidth = 2
+        plusButton.layer.borderColor = plusButton.tintColor.cgColor
+    }
+}
+
